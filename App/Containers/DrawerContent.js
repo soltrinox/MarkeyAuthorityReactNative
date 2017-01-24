5254a4cd8bdfa1a99dedbf5ad714098d3283d2c5
soltrinox <Domino123!>
2017-01-24 07:54:17 -0800

    drawer icon fixed

--- App/Containers/DrawerContent.js
+++ App/Containers/DrawerContent.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { Component } from 'react'
-import { ScrollView, Image, BackAndroid } from 'react-native'
+import { ScrollView, View, Image, BackAndroid } from 'react-native'
 import styles from './Styles/DrawerContentStyle'
 import { Images } from '../Themes'
 import DrawerButton from '../Components/DrawerButton'
@@ -53,11 +53,12 @@ class DrawerContent extends Component {
     return (
       <ScrollView style={styles.container}>
         {/*<Image source={Images.logo} style={styles.logo} />*/}
-        <DrawerButton icon='circle' text='Google PageOne' onPress={this.handlePressComponents} />
-        <DrawerButton icon='square' text='Print' onPress={this.handlePressUsage} />
-        <DrawerButton icon='check' text='Site Gaps' onPress={this.handlePressAPI} />
-        <DrawerButton icon='cog' text='Send' onPress={this.handlePressTheme} />
-        <DrawerButton icon='star' text='Settings' onPress={this.handlePressDevice} />
+        <DrawerButton icon='window-maximize' text='Google PageOne' onPress={this.handlePressComponents} />
+        <DrawerButton icon='print' text='Print' onPress={this.handlePressUsage} />
+        <DrawerButton icon='tachometer' text='Site Gaps' onPress={this.handlePressAPI} />
+        <View style={{marginBottom: 100}} ></View>
+        <DrawerButton icon='play-circle' text='Send' onPress={this.handlePressTheme} />
+        <DrawerButton icon='cog' text='Settings' onPress={this.handlePressDevice} />
       </ScrollView>
     )
   }
